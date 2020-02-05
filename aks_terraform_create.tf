3d4f02575ac80d03c2f374d77c772038c956c541
Abdul1235 <60096185+Abdul1235@users.noreply.github.com>
2020-02-05 08:01:42 +0000

    Update aks_terraform_create.tf

--- aks_terraform_create.tf
+++ aks_terraform_create.tf
@@ -5,15 +5,15 @@
 provider "azurerm" {
   # We recommend pinning to the specific version of the Azure Provider you're using
   # since new versions are released frequently
-  version = "=1.38.0"
+  #version = "=1.38.0"
 
   # More information on the authentication methods supported by
   # the AzureRM Provider can be found here:
   # http://terraform.io/docs/providers/azurerm/index.html
 
   subscription_id = "3e7223da-8235-40b2-97d1-82e996b899b7"
-  client_id       = "16a99988-88a0-4344-967c-939f2f309cc3"
-  client_secret   = ""
+  client_id       = "f2b03a90-46e5-4ee2-92b7-a9bc28bc24b5"
+  client_secret   = "DhJuNQlcRkyMhks@Y565=Qs]QjjWmV7="
   tenant_id       = "db8e2f82-8a37-4c09-b7de-ed06547b5a20"
 }
 resource "azurerm_resource_group" "main" {
